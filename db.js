a9c9d23723fc60778e1068640c237871bab35d3d
Teixeira <claudioteixeira@outlook.com>
2022-05-18 11:13:23 -0300

    Primeiro Commit

---
+++ db.js
@@ -0,0 +1,15 @@
+const burgers = require("./data/burgers");
+const ingredientes = require("./data/ingredientes");
+const opcionais = require("./data/opcionais");
+const status = require("./data/status")
+const paes = require("./data/paes")
+const carnes = require("./data/carnes")
+
+module.exports = {
+  burgers,
+  ingredientes,
+  opcionais,
+  status,
+  paes,
+  carnes
+}
